efadf509a7bc9776e5e498f7cfd9e3334b08d5f1
Pawel Sysiak <pawsys@gmail.com>
2019-10-23 13:03:37 -0700

    update

--- tools/tailwind.config.js
+++ tools/tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      sans: ['"Roboto"', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
-      serif: ['"IBM Plex Serif"', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
-      mono: ['"IBM Plex Mono"', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
-    },
+
     extend: {
       colors: {
         primary: {
-          default: '#EF52D0',
-          dark: '#9B2885',
+          default: '#777DDC',
+          dark: '#5A60C5',
         } 
       }
     }
