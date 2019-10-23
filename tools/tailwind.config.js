907d4968d37071de7b9eb0c04881899dd5adde3a
Pawel Sysiak <pawsys@gmail.com>
2019-10-23 11:52:15 -0700

    change to Brutu

--- tools/tailwind.config.js
+++ tools/tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: ['"Roboto"', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
+      serif: ['"IBM Plex Serif"', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
+      mono: ['"IBM Plex Mono"', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
+    },
     extend: {
       colors: {
-        primary: '#777DDC',
+        primary: {
+          default: '#EF52D0',
+          dark: '#9B2885',
+        } 
       }
     }
   },
